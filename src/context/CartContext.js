73eabe4bbb15bfff3366f94e65c35006135d5da2
QuyDoanVo <doanvoquy1@gmail.com>
2024-12-10 16:24:16 +0700

    ok

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -10,7 +10,15 @@ export const CartProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
 
   const addToCart = (product) => {
-    setCart((prevCart) => [...prevCart, product]);
+    setCart((prevCart) => {
+      const existingProduct = prevCart.find((item) => item.id === product.id);
+      if (existingProduct) {
+        return prevCart.map((item) =>
+          item.id === product.id ? { ...item, quantity: item.quantity + 1 } : item
+        );
+      }
+      return [...prevCart, { ...product, quantity: 1 }];
+    });
   };
 
   return (
--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,15 +1,16 @@
 import React, { useState, useEffect } from 'react';
+import { useParams } from "react-router-dom";
 
 const ProductDetail = () => {
   const [product, setProduct] = useState(null);
-
+  const { id } = useParams();
   useEffect(() => {
     // Lấy dữ liệu từ public/db.json
     fetch('/db.json')
       .then((response) => response.json())
       .then((data) => {
         // Xử lý dữ liệu
-        const product = data.products.find((p) => p.id === 1);  // Ví dụ, tìm sản phẩm với ID = 1
+        const product = data.products.find((p) => p.id === parseInt(id));
         setProduct(product);
       })
       .catch((error) => console.error('Error fetching data:', error));
